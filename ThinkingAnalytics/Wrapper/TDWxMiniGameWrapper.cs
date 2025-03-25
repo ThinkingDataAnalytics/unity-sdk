b302f3ed165b21961db5199f0d7876a488e35e72
ThinkingData <sdk@thinkingdata.cn>
2025-03-25 12:02:36 +0800

    release v3.1.3

--- ThinkingAnalytics/Wrapper/TDWxMiniGameWrapper.cs
+++ ThinkingAnalytics/Wrapper/TDWxMiniGameWrapper.cs
@@ -1,4 +1,4 @@
-#if ((!(UNITY_IOS) || UNITY_EDITOR) && (!(UNITY_ANDROID) || UNITY_EDITOR) && (!(UNITY_OPENHARMONY) || UNITY_EDITOR)) || TE_DISABLE_ANDROID_JAVA || TE_DISABLE_IOS_OC
+#if ((!(UNITY_IOS) || UNITY_EDITOR) && (!(UNITY_ANDROID) || UNITY_EDITOR) && (!(UNITY_OPENHARMONY) || UNITY_EDITOR))
 using System.Runtime.InteropServices;
 namespace ThinkingData.Analytics.Wrapper
 {
