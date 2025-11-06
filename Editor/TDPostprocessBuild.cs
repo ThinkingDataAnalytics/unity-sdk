df51159cec32a9ecd76ceefddd3bf2a44110a1b2
ThinkingData <sdk@thinkingdata.cn>
2025-11-06 10:12:39 +0800

    v3.4.0

--- Editor/TDPostprocessBuild.cs
+++ Editor/TDPostprocessBuild.cs
@@ -69,6 +69,87 @@ namespace ThinkingData.Analytics.Editors
 }
 #endif
 
+#if UNITY_OPENHARMONY
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+namespace ThinkingData.Analytics.Editors
+{
+    public class TD_PostProcessBuild
+    {
+        private static void changeContent(string path, string srcStr, string destStr)
+        {
+            string jsonContent = File.ReadAllText(path);
+            int index = jsonContent.IndexOf(srcStr);
+            if (index == -1) return;
+            File.WriteAllText(path, jsonContent.Substring(0, index) + destStr + jsonContent.Substring(index + srcStr.Length));
+        }
+        private static void ChangeFirstLineContent(string path, string srcStr, string destStr)
+        {
+            string tempPath = path + ".tmp";
+            try
+            {
+                using (var reader = new StreamReader(path))
+                using (var writer = new StreamWriter(tempPath))
+                {
+                    string firstLine = reader.ReadLine();
+                    if (firstLine != null)
+                    {
+                        int index = firstLine.IndexOf(srcStr);
+                        if (index != -1)
+                        {
+                            firstLine = firstLine.Substring(0, index) + destStr + firstLine.Substring(index + srcStr.Length);
+                        }
+                        writer.WriteLine(firstLine);
+                    }
+                    char[] buffer = new char[4096];
+                    int bytesRead;
+                    while ((bytesRead = reader.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        writer.Write(buffer, 0, bytesRead);
+                    }
+                }
+                File.Delete(path);
+                File.Move(tempPath, path);
+            }
+            finally {
+                if (File.Exists(tempPath))
+                    File.Delete(tempPath);
+            }
+        }
+        [PostProcessBuildAttribute(88)]
+        public static void OnPostProcessBuild(BuildTarget target, string targetPath)
+        {
+            string entryPath = Path.Combine(targetPath, "entry/oh-package.json5");
+            string entryBuildPath = Path.Combine(targetPath, "entry/build-profile.json5");
+            string entryRegisterPath = Path.Combine(targetPath, "entry/src/main/ets/gen/TuanjieJSScriptRegister.ets");
+            string entryProxyPath = Path.Combine(targetPath, "entry/src/main/ets/TDOpenHarmonyProxy.ts");
+            if (File.Exists(entryPath) && File.Exists(entryProxyPath) && File.Exists(entryBuildPath) && File.Exists(entryRegisterPath))
+            {
+                changeContent(entryPath, "\"TDAnalytics\"", "\"@thinkingdata/analytics\"");
+                changeContent(entryBuildPath, "\"TDAnalytics\"", "\"@thinkingdata/analytics\"");
+                changeContent(entryRegisterPath, "'TDAnalytics'", "'@thinkingdata/analytics'");
+                ChangeFirstLineContent(entryProxyPath, "} from 'TDAnalytics'", "} from '@thinkingdata/analytics'");
+                return;
+            }
+            string tuanjieLibPath = Path.Combine(targetPath, "tuanjieLib/oh-package.json5");
+            string tuanjieLibBuildPath = Path.Combine(targetPath, "tuanjieLib/build-profile.json5");
+            string tuanjieRegisterPath = Path.Combine(targetPath, "tuanjieLib/src/main/ets/gen/TuanjieJSScriptRegister.ets");
+            string tuanjieLibProxyPath = Path.Combine(targetPath, "tuanjieLib/src/main/ets/TDOpenHarmonyProxy.ts");
+            if (File.Exists(tuanjieLibPath) && File.Exists(tuanjieLibProxyPath) && File.Exists(tuanjieLibBuildPath) && File.Exists(tuanjieRegisterPath))
+            {
+                changeContent(tuanjieLibPath, "\"TDAnalytics\"", "\"@thinkingdata/analytics\"");
+                changeContent(tuanjieLibBuildPath, "\"TDAnalytics\"", "\"@thinkingdata/analytics\"");
+                changeContent(tuanjieRegisterPath,  "'TDAnalytics'", "'@thinkingdata/analytics'");
+                ChangeFirstLineContent(tuanjieLibProxyPath, "} from 'TDAnalytics'", "} from '@thinkingdata/analytics'");
+                return;
+            }
+        }
+    }
+}
+#endif
+
 #if UNITY_EDITOR && UNITY_ANDROID && UNITY_2019_1_OR_NEWER
 using UnityEditor;
 using UnityEditor.Android;
